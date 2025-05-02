52cdaa396afc568c97fda9e8e3d0e4b453561233
Vee Jay <veejay.programming@gmail.com>
2025-05-03 03:47:39 +0800

    chore: install and configure react-router-dom for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />
+    }
+]);
 
 export default function App() {
 
-    return (
-        <>
-            <Home />
-        </>
-    );
+    return <RouterProvider router={router} />;
 }
